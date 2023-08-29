8646e11093eee129b14c46d200d6fb6ab5beaa08
Bitto <68559523+LoLbitto@users.noreply.github.com>
2023-08-29 10:18:43 -0300

    Arquivos do fuzzy logic
    Arquivo FCL e o Base.java, questões sobre o que seria "baixo", "alto" e "médio" no salário e na distância ainda serão discutidos.
    Também devo citar que não sei se a participação da neuro-divergência está muito grande, tópico que pode ser debatido, atualmente ela causa um efeito de +-12%.

--- java/Base.java
+++ java/Base.java
@@ -1,5 +1,40 @@
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.*;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import javax.swing.JOptionPane;
+
 public class Base{
-	public static void main(String[] args){
-		
-	}
+    public static void main (String[] args){
+
+        //carrega o 'base.fcl'
+        FIS fis = FIS.load("base.fcl", true);
+
+        //verifica se carregou certo
+        if( fis == null ) { 
+            System.err.println("deu pau no arquivo");
+            return;
+        }
+
+        //localiza o bloco de função do "base.fcl" para usar mais tarde
+        FunctionBlock functionBlock = fis.getFunctionBlock(null);
+
+        // Dá os valores para as variáveis
+        fis.setVariable("renda", Double.parseDouble(JOptionPane.showInputDialog("Insira a renda por cabeça da familia: ")));
+        fis.setVariable("distancia", Double.parseDouble(JOptionPane.showInputDialog("Insira a distância até a escola: ")));
+        fis.setVariable("neuro", Integer.parseInt(JOptionPane.showInputDialog("o aluno possui alguma neuro divergencia? 1- sim 0- não: ")));
+
+        // Evaluate
+        fis.evaluate();
+
+        //mostra os gráficos
+        JFuzzyChart.get().chart(functionBlock);
+
+        //mostra o gráfico da situação
+        Variable situacao = functionBlock.getVariable("situacao");
+        JFuzzyChart.get().chart(situacao, situacao.getDefuzzifier(), true);
+
+        //printa o "base.fcl"
+        System.out.println(fis);
+    }
 }
